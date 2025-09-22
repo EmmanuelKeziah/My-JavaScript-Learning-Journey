9f26a18d0597e8f4b87db1e8780b0303f53c2395
Emmanuel Keziah <keziahema@gmail.com>
2025-09-23 00:33:24 +0100

    Update solutions.js

--- Variables/Exercises/solutions.js
+++ Variables/Exercises/solutions.js
@@ -1,1 +1,28 @@
 
+// Day 1 Variable Exercises - Solutions
+
+// 1. Favorite fruit
+let favoriteFruit = "Mango";
+console.log("Favorite Fruit:", favoriteFruit);
+
+// 2. Change the value
+favoriteFruit = "Orange";
+console.log("Updated Favorite Fruit:", favoriteFruit);
+
+// 3. Age
+let age = 25;
+console.log("Age:", age);
+
+// 4. Lab test tube
+let testTube = "Blood Sample";
+console.log("Test Tube:", testTube);
+
+// 5. Shopping cart
+let shoppingCart = "Apples";
+console.log("Shopping Cart:", shoppingCart);
+
+shoppingCart = "Bananas";
+console.log("Updated Shopping Cart:", shoppingCart);
+
+shoppingCart = "Grapes";
+console.log("Final Shopping Cart:", shoppingCart);
