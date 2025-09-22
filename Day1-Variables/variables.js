24d3d921fe6bebd2541bc5fc2ca37e00f955151e
Emmanuel Keziah <keziahema@gmail.com>
2025-09-23 00:56:35 +0100

    Update and rename variables.js to variables.js

---
+++ Day1-Variables/variables.js
@@ -0,0 +1,24 @@
+---
+
+## 📄 `variables.js`
+
+// Day 1: Variables (Tiny pockets of data that changed the world)
+
+// Example 1: Blood sample in a lab
+let bloodSample = "O+";
+console.log("Blood Sample:", bloodSample);
+
+// Example 2: Shopping cart at the mall
+let shoppingCart = "Apples";
+console.log("Shopping Cart:", shoppingCart);
+
+// Update value inside the cart
+shoppingCart = "Bananas";
+console.log("Updated Shopping Cart:", shoppingCart);
+
+// General variable use
+let sample = "Blood A";
+console.log("Sample:", sample);
+
+sample = "Blood B";
+console.log("Updated Sample:", sample);
